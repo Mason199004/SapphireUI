4139f8735049b0157efaa3362336fcd356ed8848
Mason Haines <mason1990haines@gmail.com>
2023-12-14 06:09:01 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,8 @@
+job("Qodana") {
+   container("jetbrains/qodana-<linter>") {
+      env["QODANA_TOKEN"] = Secrets("qodana-token")
+      shellScript {
+         content = """qodana"""
+      }
+   }
+}
